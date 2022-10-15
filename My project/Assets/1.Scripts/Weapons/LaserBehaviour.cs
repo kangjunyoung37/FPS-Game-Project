15af394a1acea8bb5c59033f7eca95842b1dd045
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-10-15 17:23:02 +0900

    #레이저 행동 구현

---
+++ My project/Assets/1.Scripts/Weapons/LaserBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class LaserBehaviour : MonoBehaviour
+{
+    #region GETTHERS
+
+    /// <summary>
+    /// 사용자 인터페이스에 쓰일 스프라이트를 리턴합니다.
+    /// </summary>
+    public abstract Sprite GetSprite();
+
+    /// <summary>
+    /// 캐릭터가 달리는 동안 레이저를 꺼야하는 경우 참을 리턴합니다.
+    /// </summary>
+    public abstract bool GetTurnOffWhileRunning();
+
+    /// <summary>
+    /// 캐릭터가 에임하는 동안 레이저를 꺼야하는 경우 참을 리턴합니다.
+    /// </summary>
+    public abstract bool GetTurnOffWhileAiming();
+
+    /// <summary>
+    /// 레이저 토글하기
+    /// </summary>
+    public abstract void Toggle();
+
+    /// <summary>
+    /// 레이저 재적용하기
+    /// </summary>
+    public abstract void Reapply();
+
+    /// <summary>
+    /// 레이저 숨기기
+    /// </summary>
+    public abstract void Hide();
+
+
+    #endregion
+}
