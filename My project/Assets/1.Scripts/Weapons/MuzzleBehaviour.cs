d121f576eb7e38e67dd9ed57bc5419e89203e229
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-10-16 23:00:33 +0900

    #Muzzle 행동 구현

---
+++ My project/Assets/1.Scripts/Weapons/MuzzleBehaviour.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MuzzleBehaviour : MonoBehaviour
+{
+    #region GETTERS
+
+    /// <summary>
+    /// 총구의 위치를 리턴합니다.
+    /// </summary>
+    public abstract Transform GetSocket();
+
+    /// <summary>
+    /// 인터페이스 쓰일 총구 Sprite를 리턴합니다.
+    /// </summary>
+    public abstract Sprite GetSprite();
+
+    /// <summary>
+    /// 발사할때 나는 사운드를 리턴합니다.
+    /// </summary>
+    public abstract AudioClip GetAudioClipFire();
+
+    /// <summary>
+    /// 발사할때 사용하기 위한 Particle를 리턴합니다.
+    /// </summary>
+    public abstract ParticleSystem GetParticleFire();
+
+    /// <summary>
+    /// 발사할 때 방출할 입자의 수를 리턴합니다.
+    /// </summary>
+    public abstract int GetParticleFireCount();
+
+    /// <summary>
+    /// 발사할때 사용되는 light 컴포넌트를 리턴합니다.
+    /// </summary>
+    public abstract Light GetFlashLight();
+
+    /// <summary>
+    /// 플래쉬가 사라지는데 걸리는 시간을 리턴합니다.
+    /// </summary>
+    public abstract float GetFlashLightDuration();
+    #endregion
+
+    #region METHODS
+    /// <summary>
+    /// 모든 총구 효과를 재생시킵니다.
+    /// </summary>
+    public abstract void Effect();
+    #endregion
+}
