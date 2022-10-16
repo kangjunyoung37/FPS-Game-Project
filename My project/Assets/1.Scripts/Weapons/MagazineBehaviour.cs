6e610b969ad3fa280e1ce634cf06de87f1f69950
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-10-16 22:59:26 +0900

    #Magazine 행동구현

---
+++ My project/Assets/1.Scripts/Weapons/MagazineBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MagazineBehaviour : MonoBehaviour
+{
+    #region GETTERS
+
+    /// <summary>
+    /// 총 탄약수를 리턴합니다.
+    /// </summary>
+    public abstract int GetAmmunitionTotal();
+
+    /// <summary>
+    /// 인터페이스에 쓰일 Sprite를 리턴합니다.
+    /// </summary>
+    public abstract Sprite GetSprite();
+    #endregion
+}
