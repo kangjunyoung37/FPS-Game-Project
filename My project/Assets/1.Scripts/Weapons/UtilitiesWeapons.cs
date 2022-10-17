51802359fde9543ff63dc0315bcd3b9cbd6fa748
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-10-17 23:35:28 +0900

    #부착물 활성화를 위한 코드 구현

---
+++ My project/Assets/1.Scripts/Weapons/UtilitiesWeapons.cs
@@ -0,0 +1,28 @@
+using InfimaGames.LowPolyShooterPack;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UtilitiesWeapons 
+{
+    /// <summary>
+    /// 한 오브젝트를 활성화하고 다른 오브젝트를 다 비활성화합니다.
+    /// </summary>
+    public static T SelectAndSetActive<T>(this T[] array , int index) where T : MonoBehaviour
+    {
+        //배열에 객체가 있는지 확인하세여!
+        if(!array.IsValid())
+        {
+            return null;
+        }
+        //모든 오브젝트 비활성화
+        array.ForrEach(obj => obj.gameObject.SetActive(false));
+        //원하는 오브젝트 활성화
+        T behaviour = array[index];
+        if(behaviour != null)
+            behaviour.gameObject.SetActive(true);
+
+        return behaviour;
+
+    }
+}
