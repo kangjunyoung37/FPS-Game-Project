a7b1b1b3f71933461023b58b119a5e992f6a4391
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-11-23 23:35:47 +0900

    #TPBoltBehaviour구현
    TP캐릭터가 Bolt Action을 할 수 있게 해줍니다.

---
+++ My project/Assets/1.Scripts/Animators/TPBoltBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPBoltBehaviour : StateMachineBehaviour
+{
+    #region FIELDS
+
+    /// <summary>
+    /// 플레이어 캐릭터Behaviour
+    /// </summary>
+    private CharacterBehaviour playerCharacter;
+
+    /// <summary>
+    /// 플레이어 인벤토리
+    /// </summary>
+    private InventoryBehaviour playerInventoryBehaviour;
+
+    #endregion
+
+    #region UNITY
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //필요한 캐릭터 컴포넌트를 받아옵니다.
+        playerCharacter ??= ServiceLocator.Current.Get<IGameModeService>().GetPlayerCharacter();
+        //인벤토리 가져오기
+        playerInventoryBehaviour = playerCharacter.GetInventory();
+
+        if (!(playerInventoryBehaviour.EquipTPWeapon() is { } weaponBehaviour))
+            return;
+        //무기 애니메이터 가져오기
+        var weaponAnimator = weaponBehaviour.gameObject.GetComponent<Animator>();
+
+        //볼트액션 애니메이션 재생
+        weaponAnimator.Play("Bolt Action");
+    }
+
+
+    #endregion
+}
