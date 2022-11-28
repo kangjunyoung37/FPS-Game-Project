33a21283c60327eec5513385ef5b7d6c663339dc
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-11-28 23:39:26 +0900

    #PlayerManager컴포넌트 구현

---
+++ My project/Assets/1.Scripts/PhotonNet/PlayerManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Start()
+    {
+       if(PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    void CreateController()
+    {
+        int x = Random.Range(0, 3);
+       
+        int z = Random.Range(0, 3);
+
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "SK_FP_CH"), new Vector3(x,0.0f,z), Quaternion.identity);
+    }
+}
