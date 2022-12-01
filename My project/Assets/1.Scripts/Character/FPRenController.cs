5a781752b9f870035c89878850b3290320c5b746
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-12-01 23:43:28 +0900

    #FPRender컨트롤러 구현

---
+++ My project/Assets/1.Scripts/Character/FPRenController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPRenController : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer[] FPRenderer;
+
+    public void FPRenOff()
+    {
+        for(int i = 0; i < FPRenderer.Length; i++)
+        {
+            FPRenderer[i].enabled = false;
+        }
+
+    }
+}
