e3334a44819d997e0884aff9e79778ce55fd6e62
kangjunyoung37 <100678932+kangjunyoung37@users.noreply.github.com>
2022-12-26 23:54:42 +0900

    #PlayerNameManager
    플레이어 닉네임 설정

---
+++ My project/Assets/1.Scripts/UI/PlayerNameManager.cs
@@ -0,0 +1,29 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerNameManager : MonoBehaviour
+{
+    [SerializeField] TMP_InputField usernameInput;
+
+    private void Start()
+    {
+        if(PlayerPrefs.HasKey("username"))
+        {
+            usernameInput.text = PlayerPrefs.GetString("username");
+            PhotonNetwork.NickName = PlayerPrefs.GetString("username");
+        }
+        else
+        {
+            usernameInput.text = "Player " + Random.Range(0, 10000).ToString("0000");
+            OnUserNameInputValueChanged();
+        }
+    }
+    public void OnUserNameInputValueChanged()
+    {
+        PhotonNetwork.NickName = usernameInput.text;
+        PlayerPrefs.SetString("username", usernameInput.text);
+    }
+}
