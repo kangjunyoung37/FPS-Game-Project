786bd0a7abd5c3e0376539ac8793cdd28ae41a7e
kangjunyoung37 <100678932+kangjunyoung37@users.noreply.github.com>
2022-12-30 23:30:01 +0900

    #InGame스크립트 수정
    1. 시간동기화
    2. 시간이 끝나면 게임이 끝나게 설정

--- My project/Assets/1.Scripts/InGame/InGame.cs
+++ My project/Assets/1.Scripts/InGame/InGame.cs
@@ -1,9 +1,12 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 public enum MapMode
 {
     DeathMatch,
@@ -30,112 +33,130 @@ public class InGame : MonoBehaviourPunCallbacks, IPunObservable
     [SerializeField]
     private Transform[] blueTransform;
 
+    [SerializeField]
+    private TMP_Text gameEndText;
     #endregion
 
     #region FIELDS
-
+    
+    private static InGame instance;
     private PhotonView PV;
-    //나중에 포톤룸 설정에서 받아올거
+    
+    //나중에 포톤룸 설정에서 받아올거   
     [SerializeField]
     private int winPoint = 0;
     [SerializeField]
     private float totalTime = 600.0f;
     [SerializeField]
-    private float rountTime;
+    private float rountTime = 180.0f;
     private float curTime;
     private float startTime;
     private int redTeamPoint = 0;
     private int blueTeamPoint = 0;
+    private bool redTeamEnter = false;
+    private bool blueTeamEnter = false;
     private bool bombinstallation = false;
-
-    [SerializeField]
+    int roomPlayerCnt;
     private bool gameStart = false;
     private bool gameEnd = false;
-    private bool firstStart = false;
+    Room currentRoom;
+
     #endregion
 
     #region PROPERTIES
 
     public int RedTeamPoint
     {
-        get
-        {
-            return redTeamPoint;
-        }
-        set
-        {
-            redTeamPoint = value;
-        }
+        get { return redTeamPoint; } 
+        set { redTeamPoint = value; }
+        
     }
 
     public int BlueTeamPoint
     {
+        get { return blueTeamPoint; }      
+        set { blueTeamPoint = value; }
+        
+    }
+
+    public bool RedTeamEnter
+    {
+        set { redTeamEnter = value; }
+    }
+
+    public bool BlueTeamEnter
+    {
+        set { blueTeamEnter = value; }
+    }
+
+    public static InGame Instance
+    {
         get
         {
-            return blueTeamPoint;
-        }
-        set
-        {
-            blueTeamPoint = value;
+            if (instance == null)
+                return null;
+            else
+                return instance;
         }
     }
 
     #endregion
 
-    private static InGame instance;
+    #region UnityMethods
     
-    public static InGame Instance
-    {
-        get {
-            if (instance == null)
-                return null;
-            else
-                return instance; 
-            }
-    }
-
     private void Awake()
     {
+        roomPlayerCnt = PhotonNetwork.CurrentRoom.PlayerCount;
+        currentRoom = PhotonNetwork.CurrentRoom;
         instance = this;
         PV = GetComponent<PhotonView>();
         scoreText.text = $" Blue :{blueTeamPoint} , Red : {RedTeamPoint}";
-
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(redTeamPoint);
-            stream.SendNext(blueTeamPoint);
-            stream.SendNext(curTime);
-            stream.SendNext(gameStart);
- 
-        }
-        else
-        {
-            redTeamPoint = (int)stream.ReceiveNext();
-            blueTeamPoint = (int)stream.ReceiveNext();
-            curTime = (float)stream.ReceiveNext();
-            gameStart = (bool)stream.ReceiveNext();
-            scoreText.text = "Blue :" + blueTeamPoint.ToString() + "Red :" + redTeamPoint.ToString();
-        }
+        
     }
 
     private void Update()
     {
-        if (gameStart && !firstStart)
+        if(PhotonNetwork.IsMasterClient)
         {
-            firstStart = true;
-            startTime = (float)System.DateTime.Now.TimeOfDay.TotalSeconds;
+            if (!gameStart && blueTeamEnter && redTeamEnter)
+            {
+                startTime = (float)System.DateTime.Now.TimeOfDay.TotalSeconds;
+                gameStart = true;
+            }
         }
-        CaculateTime();
-
-
-
+        if (gameEnd)
+            gameEndText.text = "end";
+        RoomCheckAndUpdate();
+        //데스매치일 경우
+        TimeUpdate();
     }
 
+
+    #endregion
+
     #region METHODS
+
+    private void RoomCheckAndUpdate()
+    {
+        if(currentRoom.PlayerCount != roomPlayerCnt)
+        {
+            roomPlayerCnt = currentRoom.PlayerCount;
+            Hashtable roomHash = currentRoom.CustomProperties;
+            Player[] players = PhotonNetwork.PlayerList;
+            int redCnt = 0;
+            int blueCnt = 0;
+            foreach (Player player in players)
+            {
+                if ((int)player.CustomProperties["Team"] == 1)
+                    redCnt++;
+                else
+                    blueCnt++;
+            }
+            roomHash["red"] = redCnt;
+            roomHash["blue"] = blueCnt;
+            currentRoom.SetCustomProperties(roomHash);
+        }
+    }
 
     public void GameCheck(int team)
     {
@@ -165,22 +186,80 @@ public class InGame : MonoBehaviourPunCallbacks, IPunObservable
         scoreText.text = "Blue :" + blueTeamPoint.ToString() + "Red :" + redTeamPoint.ToString();
     }
 
-    private void CaculateTime()
+    private void TimeUpdate()
     {
-        if (PhotonNetwork.IsMasterClient && gameStart)
+        if (gameStart)
         {
             curTime = (float)System.DateTime.Now.TimeOfDay.TotalSeconds - startTime;
-                  
+        }
+        if(totalTime == curTime)
+        {
+            gameEnd = true;
         }
         int minute = (int)(totalTime - curTime) / 60;
         int second = (int)(totalTime - curTime) % 60;
-
+        
         timeText.text = minute.ToString("00") +":"+ second.ToString("00");
     }
 
     private void StartGame()
     {
+        
+    }
 
+    private void GameReset()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Destroy(RoomManager.Instance.gameObject);
+        PhotonNetwork.Disconnect();
+        LoadNextScene();    
+    }
+
+    public void LoadNextScene()
+    {
+        // 비동기적으로 Scene을 불러오기 위해 Coroutine을 사용한다.
+        StartCoroutine(LoadMyAsyncScene());
+    }
+
+    IEnumerator LoadMyAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(0);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+    #endregion
+
+    #region PUNCallBack
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(redTeamPoint);
+            stream.SendNext(blueTeamPoint);
+            stream.SendNext(curTime);
+            stream.SendNext(gameStart);
+            stream.SendNext(gameEnd);
+            stream.SendNext(startTime);
+
+        }
+        else
+        {
+            redTeamPoint = (int)stream.ReceiveNext();
+            blueTeamPoint = (int)stream.ReceiveNext();
+            curTime = (float)stream.ReceiveNext();
+            gameStart = (bool)stream.ReceiveNext();
+            gameEnd = (bool)stream.ReceiveNext();
+            startTime = (float)stream.ReceiveNext();
+            scoreText.text = "Blue :" + blueTeamPoint.ToString() + "Red :" + redTeamPoint.ToString();
+        }
     }
 
     #endregion
