f5a27e842a4dc01a0a467c4c15697ef363fd66e0
kangjunyoung37 <100678932+kangjunyoung37@users.noreply.github.com>
2023-01-09 23:54:45 +0900

    #UIWeaponButton
    플레이어 창고에서 무기를 선택하는 버튼
    1. 버튼을 누르면 무기 정보를 보여줌
    2. 오른쪽 버튼을 누르면 무기를 즉시 장비

---
+++ My project/Assets/1.Scripts/UI/UIWeaponButton.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public enum WeaponType
+{
+    AR,
+    SMG,
+    SG,
+    HG,
+    SN
+}
+
+public class UIWeaponButton : MonoBehaviour , IPointerEnterHandler,IPointerExitHandler,IPointerClickHandler
+{
+    [SerializeField]
+    private WeaponType weaponType = WeaponType.AR;
+    
+    [SerializeField]
+    private string weaponName = "";
+    
+    [SerializeField]
+    private StorageManager storageManager;
+
+    [SerializeField]
+    private UIWeaponData uiWeaponData; 
+
+    private Image backGroundImage;
+    private Sprite defaultSprite;
+    private Color defalutColor;
+
+    
+    private int index;
+
+    private void Awake()
+    {
+        backGroundImage = GetComponent<Image>();
+        defaultSprite = GetComponent<Sprite>();
+        defalutColor = backGroundImage.color;
+        
+    }
+    #region Getters
+
+    public WeaponType GetWeaponType() => weaponType;
+
+    public UIWeaponData GetWeaponData() => uiWeaponData;
+
+    public string GetWeaponName() => weaponName;
+
+    #endregion
+
+    #region Properties
+
+    public int Index
+    {
+        get { return index; }
+        set { index = value; }
+    }
+
+    #endregion
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        storageManager.ButtonClickImageActive(true);
+        backGroundImage.sprite = storageManager.GetGradationSprite();
+        backGroundImage.color = Color.blue;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        storageManager.ButtonClickImageActive(false);
+        backGroundImage.sprite = defaultSprite;
+        backGroundImage.color = defalutColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left)
+        {
+            storageManager.SelectAndActive(storageManager.GetUIWeapons(), index);
+        }
+
+        if(eventData.button == PointerEventData.InputButton.Right)
+        {
+            storageManager.ClickUIWeaponButton(this);
+            storageManager.GetAudioSorce().Play();
+        }
+        storageManager.UpdateData(uiWeaponData);
+    }
+}
