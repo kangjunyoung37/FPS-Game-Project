d45fcfc0f324b48df84f63ba208393cd92cff9c2
kangjunyoung37 <100678932+kangjunyoung37@users.noreply.github.com>
2023-01-12 23:41:20 +0900

    LoadgingSceneLoader
    로딩씬을 로드해줌

---
+++ My project/Assets/1.Scripts/UI/LoadingSceneLoader.cs
@@ -0,0 +1,40 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingSceneLoader : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image progressBar;
+
+    [SerializeField]
+    private TMP_Text persentText;
+
+    private void Awake()
+    {
+        progressBar.fillAmount = 0f;
+    }
+    void Start()
+    {
+        StartCoroutine(nameof(LoadSceneProcess), (int)PhotonNetwork.CurrentRoom.CustomProperties["map"]+1);
+    }
+
+
+    IEnumerator LoadSceneProcess(int SceneNum)
+    {
+        PhotonNetwork.LoadLevel(SceneNum);
+
+        while (PhotonNetwork.LevelLoadingProgress < 1)
+        {
+            progressBar.fillAmount = PhotonNetwork.LevelLoadingProgress;
+            persentText.text = (PhotonNetwork.LevelLoadingProgress * 100.0f).ToString() + "%";
+            yield return null;
+        }
+
+    }
+
+}
