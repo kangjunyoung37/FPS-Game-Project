137f9063202ec0703aaa4ec78a3093da4e6ca966
kangjunyoung37 <100678932+kangjunyoung37@users.noreply.github.com>
2023-01-13 23:57:53 +0900

    Update IDamageable.cs
    킬로그에 적의 무기정보를 담을 수 있게 수정

--- My project/Assets/1.Scripts/Character/IDamageable.cs
+++ My project/Assets/1.Scripts/Character/IDamageable.cs
@@ -3,5 +3,5 @@ using UnityEngine.EventSystems;
 
 public interface IDamageable
 {
-    void TakeDamage(int damage, Vector3 pos, Quaternion rot, int team,int viewID, bool bullet,Transform enemy);
+    void TakeDamage(int damage, Vector3 pos, Quaternion rot, int team,int viewID, bool bullet,string enemyPlayerName, int index);
 }
