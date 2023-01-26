a8e3d6e8fb79180bcdf00bc9e4fdca23c6694dec
kangjunyoung37 <100678932+kangjunyoung37@users.noreply.github.com>
2023-01-26 23:37:25 +0900

    #LoginSystem 수정
    플레이어가 로그인에 성공했다면 PhotonNetwork Server연결

--- My project/Assets/1.Scripts/FireBase/LoginSystem.cs
+++ My project/Assets/1.Scripts/FireBase/LoginSystem.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -22,6 +23,7 @@ public class LoginSystem : MonoBehaviour
         {
             playerDataHandler.LoadData();
             MenuManager.Instance.OpenMenu("Title");
+            PhotonNetwork.ConnectUsingSettings();
         }
             
         else
