5441f3d1b6b74003f8b501bf7f3e1b1e60254695
kangjunyoung37 <100678932+kangjunyoung37@users.noreply.github.com>
2023-01-28 23:48:01 +0900

    #PlayerDatahandler수정
    1. 데이터 베이스에 유저 닉네임을 저장하는 메소드 추가
    2. 유저 닉네임 중복검사

--- My project/Assets/1.Scripts/FireBase/PlayerDataHandler.cs
+++ My project/Assets/1.Scripts/FireBase/PlayerDataHandler.cs
@@ -2,14 +2,20 @@ using Firebase.Database;
 using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using TMPro;
 using UnityEngine;
 
 public class UserData
 {
     public string deviceID;
-
-
 }
+
+public class UserName
+{
+    public string userName;
+}
+
 
 public class PlayerDataHandler : MonoBehaviour
 {
@@ -26,6 +32,7 @@ public class PlayerDataHandler : MonoBehaviour
 
     public PlayerData playerData;
     public PlayerStorageData playerStorageData;
+    public TMP_InputField nickNameInputField;
     private UserData userData = new UserData();
     
     private void Start()
@@ -39,9 +46,10 @@ public class PlayerDataHandler : MonoBehaviour
         var userId = FireBaseAuthManager.Instance.UserId;
         if (userId == string.Empty)
             return;
-
+        SaveUserNickName("", userId);
         SavePlayerData(userId);
         SavePlayerStorageData(userId);
+        
     }
 
     public void LoadData()
@@ -100,6 +108,24 @@ public class PlayerDataHandler : MonoBehaviour
             Debug.LogFormat("Load user Data in successfully {0} {1}", userId, snapshot.GetRawJsonValue());
         });
 
+        databaseRef.Child(UserDataPath).Child(userId).Child("username").GetValueAsync().ContinueWith(task =>
+        {
+            DataSnapshot snapshot = task.Result;
+
+            if (task.IsCanceled)
+            {
+                Debug.Log("Load User data was canceled");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+
+                Debug.LogError("Load user data encountered an error" + task.Exception);
+                return;
+            }
+            playerData.userName = JsonConvert.DeserializeObject<string>(snapshot.GetRawJsonValue());
+
+        });
 
     }
 
@@ -164,6 +190,46 @@ public class PlayerDataHandler : MonoBehaviour
                 return;
             }
             Debug.LogFormat("Save user data in successfully {0} {1}", userId, playerStorageData);
+        });
+
+    }
+
+    public void SaveUserNickName(string userNickName, string userID)
+    {
+   
+        Dictionary<string,object> childUpdates =new Dictionary<string,object>();
+        childUpdates["/users/" +userID + "/" +"username"] = userNickName;
+
+        databaseRef.UpdateChildrenAsync(childUpdates).ContinueWith(task =>
+        {
+            Debug.Log("Save");
+        });
+
+    }
+
+    public void NickNameCheck()
+    {
+        string userNickName = nickNameInputField.text;
+        if (userNickName == string.Empty)
+        {
+            Debug.Log("아이디 비어있음");
+        }    
+        databaseRef.Child(UserDataPath).OrderByChild("username").EqualTo(userNickName).GetValueAsync().ContinueWith(task =>
+        {
+            DataSnapshot snapshot = task.Result;
+
+            if(task.Result.ChildrenCount == 0)
+            {
+                SaveUserNickName(userNickName, FireBaseAuthManager.Instance.UserId);
+                MenuManager.Instance.OpenMenu("Title");
+                return;
+            }
+            else
+            {
+                Debug.Log("중복 아이디");
+                return;
+            }
+            
         });
 
     }
